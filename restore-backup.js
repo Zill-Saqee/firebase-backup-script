04a4557184979aaac4611f64267ed51690dc2618
Zill-Saqee <zillsaqee@gmail.com>
2022-02-21 00:34:40 -0800

    changes

--- restore-backup.js
+++ restore-backup.js
@@ -3,12 +3,12 @@ const { restore } = require("firestore-export-import");
 
 module.exports = async function restoreFromBackup(collections) {
     try {
-        console.log(collections);
+        console.log('collections to be restored ' , collections);
         await Promise.all(collections.map(async name => {
             let backupData = fs.readFileSync(__dirname + '/files/' + `${name}.json`, "utf8");
             backupData = JSON.parse(backupData);
             await restore({ [name]: backupData });
-            console.log(`collection ${name} restored to destination project`);
+            console.log(`${name} collection restored to destination project`);
         }));
     } catch (error) {
         console.log(error);
